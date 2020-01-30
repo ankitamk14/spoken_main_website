63cac5245b848f0fecf2dc46f900d770753a2149
ankita <ankitamk@gmail.com>
2020-01-30 12:56:57 +0530

    index url view

---
+++ spoken/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from django.contrib import admin
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('',views.index,name='index')
+]
--- spoken_main_website/urls.py
+++ spoken_main_website/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from spoken import views
+from django.urls import include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$',include('spoken.urls'))
 ]
