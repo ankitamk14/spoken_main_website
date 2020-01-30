3e7e90add3af2c0855436865439bbe7e3636c626
ankita <ankitamk@gmail.com>
2020-01-30 12:09:32 +0530

    code to load env variables

--- spoken_main_website/settings.py
+++ spoken_main_website/settings.py
@@ -1,3 +1,4 @@
+from dotenv import load_dotenv
 """
 Django settings for spoken_main_website project.
 
@@ -14,7 +15,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+dotenv_path = os.path.join(BASE_DIR, '.env')
+load_dotenv(dotenv_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
