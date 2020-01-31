d1fb4ee359123645765e2fda5c3e271e8c3b4e4b
ankita <ankitamk@gmail.com>
2020-01-31 18:21:15 +0530

    ui modifications

--- spoken/views.py
+++ spoken/views.py
@@ -4,5 +4,6 @@ from .models import Products,Blended_workshops
 # Create your views here.
 def index(request):
 	products = Products.objects.all()
-	context = {'products':products}
+	workshops = Blended_workshops.objects.all()
+	context = {'products':products,'workshops':workshops}
 	return render(request,'spoken/index.html',context)
